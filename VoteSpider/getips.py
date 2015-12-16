2ac05bc2639d734b2a37f1190c6f388a418df72d
oyty <oyty@oyty-mbp.local>
2015-12-16 13:50:14 +0800

    add some commond to getips.py

--- VoteSpider/getips.py
+++ VoteSpider/getips.py
@@ -6,24 +6,33 @@ import getipfuns
     配置文件
 """
 
+"""
+    以下是两种获取代理ip的策略，也就是两个网站,你可以任选一种获取ip
+"""
 strategys = {
     'stategy_haodaili': {
-        'base_url': 'http://www.haodailiip.com/guonei/',
-        'start_page': 1,
-        'end_page': 3,
-        'page_hrs':30,
+        'base_url': 'http://www.haodailiip.com/guonei/',  # 代理ip获取的网址
+        'start_page': 1,  # 获取代理ip的起始页  这个地方可以自行修改
+        'end_page': 3,  # 获取代理ip的终止页   这个地方可以自行修改
+        'page_hrs': 30,  # 表示一页有30条ip数据
         'method': 'haodaili_proxys'
     },
-    'strategy_haodaili': {
+    'strategy_kuaidaili': {
         'base_url': 'http://www.kuaidaili.com/proxylist/',
-        'start_page': 1,
-        'end_page': 5,
-        'page_hrs':10,
+        'start_page': 1,  # 获取代理ip的起始页  这个地方可以自行修改
+        'end_page': 5,  # 获取代理ip的终止页   这个地方可以自行修改
+        'page_hrs': 10,
         'method': 'kuaidaili_proxys'
     }
 }
 strategy_num = 0
-strategy = strategys.get('strategy_haodaili')
+
+"""
+    strategy_choose配置你要选择的策略，由上面可以知道
+    这两种策略为strategy_haodaili，strategy_kuaidaili
+"""
+strategy_choose = 'strategy_kuaidaili' # 这个地方可以自行修改
+strategy = strategys.get(strategy_choose)
 
 
 def execute(fun, *args):
