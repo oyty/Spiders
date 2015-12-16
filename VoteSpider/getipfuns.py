21ca4f7694c98575036fe5864c471661a00d50e0
oyty <oyty@oyty-mbp.local>
2015-12-16 12:29:56 +0800

    optimize spider with config

---
+++ VoteSpider/getipfuns.py
@@ -0,0 +1,50 @@
+# -*- coding:utf8 -*-
+import requests
+import getips
+import time
+from BeautifulSoup import BeautifulSoup
+
+"""
+这个方法是获取可用的国内代理ip，可以登入http://www.haodailiip.com/guonei/查看
+for i in range(4, 5): 主要在range里面进行修改，可以对着网站上的看，一页有三十条数据
+所以一次最好获取一页数据，range(5, 6)表示获取第四页数据，以此类推
+"""
+
+of = open('proxy.txt', 'w')
+base_url = getips.strategy.get('base_url')
+
+
+def haodaili_proxys(start_page, end_page):
+    for i in range(start_page, end_page):
+        url = base_url + str(i)
+        print "正在采集" + url
+        html = requests.get(url).text
+        bs = BeautifulSoup(html)
+        table = bs.find('table', {"class": "proxy_table"})
+        tr = table.findAll('tr')
+        for i in range(0, int(getips.strategy.get('page_hrs'))):
+            td = tr[i].findAll('td')
+            proxy_ip = td[0].text.strip()
+            proxy_port = td[1].text.strip()
+            of.write('%s:%s\n' % (proxy_ip, proxy_port))
+            print '%s:%s\n' % (proxy_ip, proxy_port)
+        time.sleep(2)
+    of.close()
+
+
+def kuaidaili_proxys(start_page, end_page):
+    for i in range(start_page, end_page):
+        url = base_url + str(i)
+        print "正在采集" + url
+        html = requests.get(url).text
+        bs = BeautifulSoup(html)
+        table = bs.find('tbody')
+        tr = table.findAll('tr')
+        for i in range(0, int(getips.strategy.get('page_hrs'))):
+            td = tr[i].findAll('td')
+            proxy_ip = td[0].text.strip()
+            proxy_port = td[1].text.strip()
+            of.write('%s:%s\n' % (proxy_ip, proxy_port))
+            print '%s:%s\n' % (proxy_ip, proxy_port)
+        time.sleep(2)
+    of.close()
--- VoteSpider/getips.py
+++ VoteSpider/getips.py
@@ -1,28 +1,36 @@
-# -*- coding:utf8 -*-
-import requests
-import time
-from BeautifulSoup import BeautifulSoup
+# coding=utf-8
+__author__ = 'oyty'
+import getipfuns
 
 """
-这个方法是获取可用的国内代理ip，可以登入http://www.haodailiip.com/guonei/查看
-for i in range(4, 5): 主要在range里面进行修改，可以对着网站上的看，一页有三十条数据
-所以一次最好获取一页数据，range(5, 6)表示获取第四页数据，以此类推
+    配置文件
 """
 
-of = open('proxy.txt', 'w')
-url = 'http://www.haodailiip.com/guonei/'
-for i in range(5, 6):
-    Url = 'http://www.haodailiip.com/guonei/' + str(i)
-    print "正在采集" + Url
-    html = requests.get(Url).text
-    bs = BeautifulSoup(html)
-    table = bs.find('table', {"class": "proxy_table"})
-    tr = table.findAll('tr')
-    for i in range(1, 31):
-        td = tr[i].findAll('td')
-        proxy_ip = td[0].text.strip()
-        proxy_port = td[1].text.strip()
-        of.write('%s:%s\n' % (proxy_ip, proxy_port))
-        print '%s:%s\n' % (proxy_ip, proxy_port)
-    time.sleep(2)
-    of.closed
+strategys = {
+    'stategy_haodaili': {
+        'base_url': 'http://www.haodailiip.com/guonei/',
+        'start_page': 1,
+        'end_page': 3,
+        'page_hrs':30,
+        'method': 'haodaili_proxys'
+    },
+    'strategy_haodaili': {
+        'base_url': 'http://www.kuaidaili.com/proxylist/',
+        'start_page': 1,
+        'end_page': 5,
+        'page_hrs':10,
+        'method': 'kuaidaili_proxys'
+    }
+}
+strategy_num = 0
+strategy = strategys.get('strategy_haodaili')
+
+
+def execute(fun, *args):
+    fun(args[0], args[1])
+
+
+if __name__ == '__main__':
+    start_page = int(strategy.get('start_page'))
+    end_page = int(strategy.get('end_page'))
+    execute(getattr(getipfuns, strategy.get('method')), start_page, end_page)
